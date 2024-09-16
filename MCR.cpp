3960fba3664403d3f21f66da80b87f327274f1ae
Wei-zwd <70091369+zwdconstrucition@users.noreply.github.com>
2024-09-16 19:39:51 +0800

    Update MCR.cpp
    modify judge condition in case of "no winner".

--- MCR.cpp
+++ MCR.cpp
@@ -47,8 +47,7 @@ cout << "Win!" << endl;
 break; // need to terminate the problem
 }
 }
-if (i==3) // all celles with i=0 to 2 have been inputted above but no
-winner yet
+if (n==9&&isWin()==FALSE) // all celles with i=0 to 2 have been inputted above but no winner yet
 cout << "Tie!" << endl;
 // show the game to console
 cout << game[0][0] << " " << game[0][1] << " " << game[0][2] << endl;
