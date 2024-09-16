1d09c3fc6f68beb69f9ecc13449028ada6362f7c
yyxzz2 <107185856+yyxzz2@users.noreply.github.com>
2024-09-16 20:04:39 +0800

    Update MCR.cpp

--- MCR.cpp
+++ MCR.cpp
@@ -1,3 +1,5 @@
+#include<bits/stdc++.h>
+using namespace std;
 bool isWin(char game[3][3]){
 bool win = false;
 if (game[0][0] == game[0][1] && game[0][1] == game[0][2] && (game[0][0]
@@ -22,6 +24,7 @@ return win;
 }
 int main(){
 int i, j;
+int t[3][3]={0};//Check whether player can use this position,0 for not
 char game[3][3] = {' '}; // Tic-tac-toe
 char player1 = 'X';
 char player2 = 'O';
@@ -35,8 +38,14 @@ if (turn == false)
 cout << "Player 1: ";
 else
 cout << "Player 2: ";
-cout << "Which cell to mark? i:[1..3], j:[1..3]: ";
+cout << "Which cell to mark? i:[0..2], j:[0..2]: ";//should be 0-2，not 1-3
 cin >> i >> j;
+if((i!=1 && i!=2 && i!=0) || (j!=1 && j!=2 && j!=0))
+  cout << "illegal input,the game over!";
+  break;
+if (t[i][j]==1) 
+{cout<<"illegal input ";
+  else(t[i][j]=1);
 if (turn == false)
 game[i][j] = 'X';
 10 Modern Code Review Exercise CS5351 2024/2025
